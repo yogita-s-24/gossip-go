a4779f364ac35e20a04333d861d143c714d6f7da
Yogita Shete <yogitashete24@gmail.com>
2025-03-18 18:52:52 +0530

    feat : create the routes for signup, login and logout

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export const signup = (req, res) =>{
+    res.send("Signup Route");
+
+    
+}
+
+export const login = (req, res) =>{
+    res.send("Login Route")
+}
+
+export const logout = (req, res) =>{
+    res.send("Logout Route")
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
 const PORT = 5000;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
