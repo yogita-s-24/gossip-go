491ab149c9303c2137377a7b6b4b09ca3d847997
Yogita Shete <yogitashete24@gmail.com>
2025-03-20 18:51:52 +0530

    feat: Implement user logout by clearing JWT cookie

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -82,5 +82,11 @@ export const login = async (req, res) =>{
 }
 
 export const logout = async (req, res) =>{
-    res.send("Logout Route")
-}
+   try {
+    res.cookie("jwt", "", {maxAge : 0});
+    res.status(200).json({message: "Logged out successfully"});
+   } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({message:"Internal Server Error."})
+   }
+};
