44363857bf4cf475e740ce5ceb51c1558c90c85d
Yogita Shete <yogitashete24@gmail.com>
2025-03-20 18:46:10 +0530

    feat: Implement user login with JWT authentication

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -50,10 +50,37 @@ export const signup = async (req, res) =>{
         }
 }
 
-export const login = (req, res) =>{
-    res.send("Login Route")
+export const login = async (req, res) =>{
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message : "Invalid credentials"});
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json ({message :"Invalid credentials"});
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id : user._id,
+            fullName : user.fullName,
+            email : user.email,
+            profilePic: user.profilePic
+        })
+
+    }catch(error){
+      console.log("Error in login controller", error.message);
+      return res.status(500).json({message :"Internal Server Error"});
+    }
+    
 }
 
-export const logout = (req, res) =>{
+export const logout = async (req, res) =>{
     res.send("Logout Route")
 }
