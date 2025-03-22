da24c50c17fde28501d13d7f5d5dbd254449a188
Yogita Shete <yogitashete24@gmail.com>
2025-03-22 23:33:26 +0530

    feat : create the protect route

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile)
 
 export default router;
