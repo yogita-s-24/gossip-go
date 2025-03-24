812afc2858c4e41cbff464cfe380b9640a56f1f3
Yogita Shete <yogitashete24@gmail.com>
2025-03-24 16:13:35 +0530

    Added profile update functionality with Cloudinary image upload

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs'
 import User from '../models/user.model.js';
 import { generateToken } from '../lib/utils.js';
+import cloudinary from '../lib/cloudinary.js';
 export const signup = async (req, res) =>{
 
     const {fullName,email,password,profilePic} = req.body;
@@ -90,3 +91,27 @@ export const logout = async (req, res) =>{
     res.status(500).json({message:"Internal Server Error."})
    }
 };
+
+export const updateProfile = async (req, res) =>{
+    try {
+        const {profilePic} = req.body;
+
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message: "Profile Pic is required."})
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findIdAndUpdate(
+            userId,
+            { profilePic : uploadResponse.secure_url},
+            { new : true}
+        );
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("Error in update profile : error");
+        res.status(500).json({ message : "Internal server error"});
+    }
+}
