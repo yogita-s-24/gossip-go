72746ca0e0234826e8e63bd61d2ee5b14e86231c
Yogita Shete <yogitashete24@gmail.com>
2025-04-13 12:51:49 +0530

    add tailwind css and daisyui

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
 
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <App />
-    <h1>Hello</h1>
+    <p class="text-3xl font-bold text-green-600 underline">
+    Hello world!
+  </p>
+  
   </>
 )
