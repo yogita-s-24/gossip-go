6fdd0c91ab3693ac8b7d3586435d46a411d463b8
Yogita Shete <yogitashete24@gmail.com>
2025-04-13 17:50:52 +0530

    ref : add validations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './components/Navbar';
 
 import HomePage from './pages/HomePage';
@@ -8,17 +8,34 @@ import ProfilePage from './pages/ProfilePage';
 import SettingPage from './pages/SettingPage';
 
 import {Routes, Route } from 'react-router-dom';
+import { useAuthStore } from './store/useAuthStore';
+
+import {Loader} from "lucide-react";
 
 function App() {
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+
+  useEffect (() =>{
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log(authUser);
+
+  if(isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin"/>
+    </div>
+  )
+
   return (
     <div>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/signup" element={<SignupPage/>}/>
-        <Route path="/profile" element={<ProfilePage/>}/>
+        <Route path="/" element={ authUser ? <HomePage/> : <Navigate to="/login"/>}/>
+        <Route path="/signup" element={ authUser ? <SignupPage/>  : <Navigate to="/"/>}/>
+        <Route path="/login" element={ authUser ? <LoginPage/>  : <Navigate to="/"/>}/>
         <Route path="/setting" element={<SettingPage/>}/>
+        <Route path="/profile" element={ authUser ? <ProfilePage/>  : <Navigate to="/"/>}/>
 
       </Routes>
      
--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,6 +1,24 @@
 import {create} from "zustand";
+import { axiosInstance } from "../lib/axios";
 
 export const useAuthStore = create((set) => ({
     authUser : null,
-    isCheckingAuth : true
+    isCheckingAuth : true,
+    isSigningup : false,
+    isLoggingIng : false,
+    isUpdatingProfile : false,
+    checkAuth : async () =>{
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({authUer : res.data});
+        } catch (error) {
+            console.log("Error in checkAuth : ", error);
+            set({authUser : null});
+        }
+        finally{
+            set({
+                isCheckingAuth : false
+            });
+        }
+    }
 }))
