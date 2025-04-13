fcb2a1315f47fc51e93a7b87f500f2be2bc25597
Yogita Shete <yogitashete24@gmail.com>
2025-04-13 17:05:34 +0530

    add cors middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+
 
 dotenv.config();
 
@@ -13,6 +15,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(
+    origin : "http://localhost:3000",
+    credentials : true
+))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
