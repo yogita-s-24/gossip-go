e4a400eaa6e3ebdde00be8c8d81821a5963d2ef6
Yogita Shete <yogitashete24@gmail.com>
2025-04-14 23:45:47 +0530

    create the signup functionality

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
 
 const SignupPage = () => {
-  return (
-    <div>
-      SignupPage
-    </div>
-  )
-}
+  const [showPassword, setPassword] = useState(false);
+  const [showConfirmPassword, setConfirmPassword] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
 
-export default SignupPage
+  const { signup, isSigningUp } = useAuthStore();
+
+  const validateForm = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="min-h=screen grid lg:grid-cols-2">
+      {/* left side */}
+      <div className="flex flex-col items-center justify-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
+
+          {/* Logo */}
+          <div className="text-center mb-8">
+            <div className="flex flex-col item-center gap-2 group">
+              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+                <MessageSquare className="size-6 text-primary" />
+              </div>
+
+              <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+              <p className="text-base-content/60">
+                Get started with your free account.
+              </p>
+            </div>
+          </div>
+
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Full Name</span>
+              </label>
+
+              <div className="absolute insert-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <User className="size-5 text-base-content/40"/>
+              </div>
+
+              <input type="text" 
+              className={`input input-bordered w-full pl-10`}
+              placeholder="John Doe"
+              value={FormData.fullName}
+              onChange={(e) => setFormData({...FormData, fullName:e.target.value})}
+               />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
