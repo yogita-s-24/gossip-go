455a4579d2b063cce7297581be3a5137e8ab7e81
Yogita Shete <yogitashete24@gmail.com>
2025-04-16 18:17:45 +0530

    fix(auth): correct route guards and fix typo in /settings path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,54 @@
-import React, { useEffect } from 'react';
-import Navbar from './components/Navbar';
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
 
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import ProfilePage from './pages/ProfilePage';
-import SettingPage from './pages/SettingPage';
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingPage from "./pages/SettingPage";
 
-import {Routes, Route } from 'react-router-dom';
-import { useAuthStore } from './store/useAuthStore';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
 
-import {Loader} from "lucide-react";
+import { Loader } from "lucide-react";
 
 function App() {
-  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
-  useEffect (() =>{
+  useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  console.log(authUser);
-
-  if(isCheckingAuth && !authUser) return (
-    <div className="flex items-center justify-center h-screen">
-      <Loader className="size-10 animate-spin"/>
-    </div>
-  )
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={ authUser ? <HomePage/> : <Navigate to="/login"/>}/>
-        <Route path="/signup" element={ authUser ? <SignupPage/>  : <Navigate to="/"/>}/>
-        <Route path="/login" element={ authUser ? <LoginPage/>  : <Navigate to="/"/>}/>
-        <Route path="/setting" element={<SettingPage/>}/>
-        <Route path="/profile" element={ authUser ? <ProfilePage/>  : <Navigate to="/"/>}/>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignupPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
 
+        <Route path="/settings" element={<SettingPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/" />}
+        />
       </Routes>
-     
     </div>
   );
 }
