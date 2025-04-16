96964d76fb6347ebeece1e4ab7ec34eaecc2c4e3
Yogita Shete <yogitashete24@gmail.com>
2025-04-16 18:16:22 +0530

    feat(server): set up Express app with routes, CORS, and DB connection

--- server/src/index.js
+++ server/src/index.js
@@ -16,9 +16,11 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
-    origin : "http://localhost:3000",
+   {
+    origin : "http://localhost:5173",
     credentials : true
-))
+   }
+));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
