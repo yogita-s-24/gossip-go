24f3125da6599ff3a253fab0484330c3ec3d0786
Yogita Shete <yogitashete24@gmail.com>
2025-04-26 22:48:35 +0530

    fix: correct layout structure and conditional rendering on HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {create} from "zustand";
+import SideBar from '../components/SideBar';
+import NoChatSelected from '../components/NoChatSelected';
+import ChatContainer from '../components/ChatContainer';
+import { useChatStore } from '../store/useChatStore';
 
 const HomePage = () => {
+const {selectedUser} = useChatStore();
+
   return (
-    <div>
-      HomePage
+    <div className="h-screen bg-base-200">
+      <div className="flex item-center justifu-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <SideBar />
+
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default HomePage
+export default HomePage;
