301302d8a48b20b67dfd5a6b6b12266aa7bf33fe
Yogita Shete <yogitashete24@gmail.com>
2025-04-30 23:07:04 +0530

    correct the route

--- server/src/index.js
+++ server/src/index.js
@@ -23,7 +23,7 @@ app.use(cors(
 ));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
